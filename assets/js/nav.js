64f1cbe772efbec5e63eebe095c2731262902e1f
Rosemary <59313516+Nwosu-Ihueze@users.noreply.github.com>
2020-11-12 17:19:44 -0500

    linked resume

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,11 +1,15 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
+
     document.getElementById("myNav").style.width = "250px";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
 function closeNav() {
+    $('.navbar-collapse a').click(function() {
+        $(".navbar-collapse").collapse('hide');
+    });
     document.getElementById("myNav").style.width = "0";
     document.body.style.backgroundColor = "white";
 }
